530953c2cc0eceb0f791351340efb2e5a6ce2062
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 00:53:14 +0300

    Scripted auto-commit on change (2021-12-03 00:53:14) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportStatApp.java
+++ src/main/java/ru/bmstu/lab3/AirportStatApp.java
@@ -1,6 +1,8 @@
 package main.java.ru.bmstu.lab3;
 
 
+import org.apache.spark.SparkConf;
+
 public class AirportStatApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().
