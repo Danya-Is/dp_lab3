487d7889ba25543716617830d3b8954d6b4839e8
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 20:46:49 +0300

    Scripted auto-commit on change (2021-12-03 20:46:49) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportStatApp.java
+++ src/main/java/ru/bmstu/lab3/AirportStatApp.java
@@ -24,7 +24,7 @@ public class AirportStatApp {
         SparkConf conf = new SparkConf().setAppName("AirportStatApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("FLIGHTS.scv").filter(row -> !row.contains("ARR_DELAY"));
-        JavaRDDflights
+        JavaRDD<> flights
                 .map(flightRow -> flightRow.split(FLIGHTS_DELIMITER))
                 .mapToPair(flightRow -> new Tuple2<>(new Tuple2<>(flightRow[DEPATURE_AIRPORT_POS], flightRow[DESTINATION_AIRPORT_POS]), flightRow[AIRPORT_DELAY_POS]))
                 .combineByKey(new CreateRouteInfoFunction(), new AppendRouteInfoFunction(), new MergeRouteInfoFunction());
