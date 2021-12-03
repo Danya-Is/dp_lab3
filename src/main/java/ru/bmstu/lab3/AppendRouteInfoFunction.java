479cbbccdd0ac7b1ce8e3c25a43e568e33f67f5b
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 20:44:38 +0300

    Scripted auto-commit on change (2021-12-03 20:44:38) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AppendRouteInfoFunction.java
+++ src/main/java/ru/bmstu/lab3/AppendRouteInfoFunction.java
@@ -2,9 +2,14 @@ package ru.bmstu.lab3;
 
 import org.apache.spark.api.java.function.Function2;
 
-public class AppendRouteInfoFunction implements Function2<RouteInfo, String, RouteInfo> {
+public class AppendRouteInfoFunction implements Function2<String, RouteInfo, String> {
     @Override
     public RouteInfo call(RouteInfo routeInfo, String delay) throws Exception {
         return routeInfo.addFlight(RouteInfo.createRoute(delay));
+    }
+
+    @Override
+    public String call(String s, RouteInfo routeInfo) throws Exception {
+        return null;
     }
 }
