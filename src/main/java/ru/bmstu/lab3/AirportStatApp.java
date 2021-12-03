8e6aae0cf9449e86e3841d0bc93b18b59c00a75b
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 21:00:14 +0300

    Scripted auto-commit on change (2021-12-03 21:00:14) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportStatApp.java
+++ src/main/java/ru/bmstu/lab3/AirportStatApp.java
@@ -25,14 +25,16 @@ public class AirportStatApp {
         SparkConf conf = new SparkConf().setAppName("AirportStatApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("FLIGHTS.scv").filter(row -> !row.contains("ARR_DELAY"));
-        JavaRDD<Tuple2<String, String>> airports = sc.textFile("AIRPORTS.scv")
+        JavaPairRDD<Object, Object> airports = sc.textFile("AIRPORTS.scv")
                 .filter(row -> !row.contains("Code"))
                 .map(airportRow -> airportRow.split(AIRPORTS_DELIMITER))
-                .map(airportRows -> new Tuple2<>(airportRows[0], airportRows[1]));
+                .mapToPair(airportRows -> new Tuple2<>(airportRows[0], airportRows[1]));
         JavaPairRDD<Tuple2<String, String>, RouteInfo> routes = flights
                 .map(flightRow -> flightRow.split(FLIGHTS_DELIMITER))
                 .mapToPair(flightRows -> new Tuple2<>(new Tuple2<>(flightRows[DEPATURE_AIRPORT_POS], flightRows[DESTINATION_AIRPORT_POS]), flightRows[AIRPORT_DELAY_POS]))
                 .combineByKey(new CreateRouteInfoFunction(), new AppendRouteInfoFunction(), new MergeRouteInfoFunction());
+
+        airports.collectAsMap();
 
 
     }
