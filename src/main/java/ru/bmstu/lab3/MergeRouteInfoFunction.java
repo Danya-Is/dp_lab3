0d7a765004ff48f3779da2dffdf7eafa0b5e7bee
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 20:39:17 +0300

    Scripted auto-commit on change (2021-12-03 20:39:17) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/MergeRouteInfoFunction.java
+++ src/main/java/ru/bmstu/lab3/MergeRouteInfoFunction.java
@@ -1,4 +1,10 @@
 package ru.bmstu.lab3;
 
-public class MergeRouteInfo {
+import org.apache.spark.api.java.function.Function2;
+
+public class MergeRouteInfoFunction implements Function2<RouteInfo, RouteInfo, RouteInfo> {
+    @Override
+    public RouteInfo call(RouteInfo routeInfo, RouteInfo routeInfo2) throws Exception {
+        return null;
+    }
 }
