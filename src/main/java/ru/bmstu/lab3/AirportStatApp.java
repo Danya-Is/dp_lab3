b1a9dffd9f85a8bfb0c6c9570dfc27add710bd7e
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 18:03:20 +0300

    Scripted auto-commit on change (2021-12-03 18:03:20) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportStatApp.java
+++ src/main/java/ru/bmstu/lab3/AirportStatApp.java
@@ -2,12 +2,14 @@ package main.java.ru.bmstu.lab3;
 
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportStatApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("AirportStatApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.textFile("FLIGHTS.scv");
+        JavaRDD<String> flights = sc.textFile("FLIGHTS.scv");
+
     }
 }
