8416d72e5201dc2e3a4e5cab6a54a9903f26db5b
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 20:24:54 +0300

    Scripted auto-commit on change (2021-12-03 20:24:54) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/CreateRouteInfo.java
+++ src/main/java/ru/bmstu/lab3/CreateRouteInfo.java
@@ -5,7 +5,7 @@ import org.apache.spark.api.java.function.Function;
 public class CreateRouteInfo extends Function<String, RouteInfo> {
 
     @Override
-    public RouteInfo call(String s) throws Exception {
+    public RouteInfo call(String delay) throws Exception {
         return ;
     }
 }
