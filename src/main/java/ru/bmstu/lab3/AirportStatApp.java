974604b63ca96b42218605b8d00a76745d6f0ca9
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 18:39:53 +0300

    Scripted auto-commit on change (2021-12-03 18:39:53) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportStatApp.java
+++ src/main/java/ru/bmstu/lab3/AirportStatApp.java
@@ -10,11 +10,12 @@ public class AirportStatApp {
     public static final int DEPATURE_AIRPORT_POS = 11;
     public static final int DESTINATION_AIRPORT_POS = 14;
     public static final int AIRPORT_DELAY_POS = 18;
+    public static final int IS_CANCELED_POS = 19;
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("AirportStatApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> flights = sc.textFile("FLIGHTS.scv");
+        JavaRDD<String> flights = sc.textFile("FLIGHTS.scv").filter(row -> row.contains(""));
 
     }
 }
