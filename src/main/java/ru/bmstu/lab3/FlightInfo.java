27f4e51ed58399998d593ab3c8983f950292145f
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-03 18:53:10 +0300

    Scripted auto-commit on change (2021-12-03 18:53:10) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/FlightInfo.java
+++ src/main/java/ru/bmstu/lab3/FlightInfo.java
@@ -3,6 +3,15 @@ package ru.bmstu.lab3;
 import java.io.Serializable;
 
 public class FlightInfo implements Serializable {
+    private float maxDelay;
+    private int flightAmount;
+    private int delayedAmount;
+    private int canceledAmount;
 
-    public FlightInfo(String maxDelay)
+    public FlightInfo(float maxDelay, int flightAmount, int delayedAmount, int canceledAmount) {
+        this.maxDelay = maxDelay;
+        this.flightAmount = flightAmount;
+        this.delayedAmount = delayedAmount;
+        this.canceledAmount = canceledAmount;
+    }
 }
